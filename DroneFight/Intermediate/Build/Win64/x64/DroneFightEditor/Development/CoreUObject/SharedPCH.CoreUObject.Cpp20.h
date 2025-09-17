c7e7c84215347fbb88d3c585cac1f99ddda61918
sangmin <sangsang366@gmail.com>
2025-09-17 11:27:10 +0900

    Remove tracked intermediate files

--- DroneFight/Intermediate/Build/Win64/x64/DroneFightEditor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h
-#include "D:/Unreal_6-1/DroneFight/DroneFight/Intermediate/Build/Win64/x64/DroneFightEditor/Development/CoreUObject/SharedDefinitions.CoreUObject.Cpp20.h"
-#include "Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h"
--- DroneFight/Intermediate/Build/Win64/x64/DroneFightEditor/Development/UnrealEd/SharedPCH.UnrealEd.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
-#include "D:/Unreal_6-1/DroneFight/DroneFight/Intermediate/Build/Win64/x64/DroneFightEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
-#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
